eea6d503929629f0c357ee570acbdb3b1ee92941
insanemike <insanemike@2bf686fb-281d-0410-880b-5de99ea81013>
2006-09-12 05:10:24 +0000

    initial import

---
+++ trunk/hw1/lmu/cs/msutton/geometry/Point.java
@@ -0,0 +1,5 @@
+package edu.lmu.cs.msutton.geometry;
+
+public class Point {
+
+}
---
+++ trunk/hw1/lmu/cs/msutton/university/Person.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.lmu.cs.msutton.university;
+
+/**
+ * @author Michael Sutton
+ *
+ */
+public class Person {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
