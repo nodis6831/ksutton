969f9cf5d1244e20e6a0b41526ffada1df73ff1d
garrett.shannon <garrett.shannon@2bf686fb-281d-0410-880b-5de99ea81013>
2006-09-14 06:17:51 +0000

    Pretty much done.  Rank and Suit are enums.  NOt sure how trying to compare ranks will play out.

--- hw1/edu/lmu/cs/msutton/games/Card.java
+++ hw1/edu/lmu/cs/msutton/games/Card.java
@@ -1,5 +1,33 @@
 package edu.lmu.cs.msutton.games;
 
 public class Card {
+	public enum Suit {
+		SPADES, HEARTS, DIAMONDS, CLUBS;
+	}
 
+	public enum Rank {
+		ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING;
+	}
+
+	private Suit suit;
+
+	private Rank rank;
+
+	public Card(Suit s, Rank r) {
+		this.suit = s;
+		this.rank = r;
+	}
+
+	public Rank getRank() {
+		return rank;
+	}
+
+	public Suit getSuit() {
+		return suit;
+	}
+
+	@Override
+	public String toString() {
+		return this.rank + " of " + this.suit;
+	}
 }
