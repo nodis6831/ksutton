d283599a9fd087cbdb07ef15382d7d1d6156cd55
insanemike <insanemike@2bf686fb-281d-0410-880b-5de99ea81013>
2006-09-14 03:52:36 +0000

    filled out Point class to include all required functionality added simple unit tests to the main()

--- hw1/edu/lmu/cs/msutton/geometry/Point.java
+++ hw1/edu/lmu/cs/msutton/geometry/Point.java
@@ -1,5 +1,71 @@
+/*
+ * A simple immutable Point class
+ * Kelly Sutton, Garrett Shannon
+ * CMSI 281, Fall 2006
+ */
+
 package edu.lmu.cs.msutton.geometry;
 
 public class Point {
 
+	private double x;
+
+	private double y;
+
+	public static final Point ORIGIN = new Point(0, 0);
+
+	/**
+	 * @param x
+	 * @param y
+	 */
+	public Point(double x, double y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+
+	/**
+	 * @return the x
+	 */
+	public double getX() {
+		return x;
+	}
+
+	/**
+	 * @return the y
+	 */
+	public double getY() {
+		return y;
+	}
+
+	/**
+	 * @return true iff this == that
+	 */
+	public boolean equals(Object that) {
+
+		if (that instanceof Point) {
+
+			Point p = (Point) that;
+
+			if ((this.x == p.x) && (this.y == p.y)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Calling the main of this Point class runs the unit tests
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO move unit tests to LineTest.java
+		Point p = new Point(0, 0);
+		Point q = new Point(10, 10);
+
+		assert p.equals(ORIGIN);
+		assert !q.equals(ORIGIN);
+	}
 }
