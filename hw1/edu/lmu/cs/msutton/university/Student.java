2642b7e522a0c5573352fd794012c1e2255555d3
garrett.shannon <garrett.shannon@2bf686fb-281d-0410-880b-5de99ea81013>
2006-09-14 04:35:26 +0000

    Beggings of student.  Problems with transcript toString

--- hw1/edu/lmu/cs/msutton/university/Student.java
+++ hw1/edu/lmu/cs/msutton/university/Student.java
@@ -1,5 +1,51 @@
 package edu.lmu.cs.msutton.university;
 
-public class Student {
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Garrett Shannon
+ * @author Kelly Sutton
+ * 
+ */
+public class Student extends Person {
+	private String country;
+
+	private int credits;
+
+	private double gpa;
+
+	private final Transcript transcript = new Transcript();
+
+	public Student(String name, boolean male, int yob, String country) {
+		super(name, male, yob);
+		this.country = country;
+	}
+
+	public void updateTranscript(Section section, Grade grade) {
+		transcript.add(section, grade);
+	}
+
+	public void printTranscript() {
+		System.out.println(transcript);
+	}
+
+	private class Transcript {
+		private List sectionList = new ArrayList();
+
+		private List gradeList = new ArrayList();
+
+		void add(Section section, Grade grade) {
+			sectionList.add(section);
+			gradeList.add(grade);
+		}
+
+		public String toString() {
+			List tmp = new ArrayList;
+			s.
+			
+			return sectionList.toString() + gradeList.toString()
+		}
+	}
 
 }
