05358ffd6bb9ee0cee7f8e61065a79ab754f254e
garrett.shannon <garrett.shannon@2bf686fb-281d-0410-880b-5de99ea81013>
2006-09-14 05:20:09 +0000

    Just a string.  Don;t know if we need other features.

--- trunk/hw1/edu/lmu/cs/msutton/university/Section.java
+++ trunk/hw1/edu/lmu/cs/msutton/university/Section.java
@@ -1,5 +1,13 @@
 package edu.lmu.cs.msutton.university;
 
 public class Section {
+	private String section;
 
+	public Section(String s) {
+		this.section = s;
+	}
+
+	public String getSection() {
+		return section;
+	}
 }
