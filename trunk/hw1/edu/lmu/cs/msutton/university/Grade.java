b6aec1e75cba80d4d061d94a0e4338e20d863bd1
garrett.shannon <garrett.shannon@2bf686fb-281d-0410-880b-5de99ea81013>
2006-09-14 05:17:09 +0000

    Grade is simply a double.  Don't know if we need to add something which will convert it to a letter grade.

--- trunk/hw1/edu/lmu/cs/msutton/university/Grade.java
+++ trunk/hw1/edu/lmu/cs/msutton/university/Grade.java
@@ -1,5 +1,13 @@
 package edu.lmu.cs.msutton.university;
 
 public class Grade {
+	private double grade;
 
+	public Grade(double g) {
+		this.grade = g;
+	}
+
+	public double getGrade() {
+		return grade;
+	}
 }
