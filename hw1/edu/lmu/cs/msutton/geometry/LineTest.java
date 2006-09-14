1824f860736dbd2188a1c5c8eb63009c25d13936
michael.k.sutton <michael.k.sutton@2bf686fb-281d-0410-880b-5de99ea81013>
2006-09-14 06:26:44 +0000

    added comments

--- hw1/edu/lmu/cs/msutton/geometry/LineTest.java
+++ hw1/edu/lmu/cs/msutton/geometry/LineTest.java
@@ -17,15 +17,12 @@ public class LineTest {
 		Line a = new Line(Point.ORIGIN, 4, 3);
 		Line b = new Line(Point.ORIGIN, 1, 0);
 		
-		System.out.println( b.toString() );
-		b.rotateLine( Math.PI / 4 );
-		System.out.println( b.toString() );
+		b.rotateLine(Math.PI / 4); //rotate 45 d
 		
 		assert m.equals(n);
 		assert m.equals(b);
 		assert n.equals(b);
 		assert !a.equals(m);
-		
 
 		System.out.println("Complete.");
 	}
