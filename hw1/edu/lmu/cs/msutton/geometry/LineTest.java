77359b5f5dd6464ba964deed5995e2eafe47e932
michael.k.sutton <michael.k.sutton@2bf686fb-281d-0410-880b-5de99ea81013>
2006-09-14 06:23:58 +0000

    Added test with rotation

--- hw1/edu/lmu/cs/msutton/geometry/LineTest.java
+++ hw1/edu/lmu/cs/msutton/geometry/LineTest.java
@@ -15,9 +15,17 @@ public class LineTest {
 		Line m = new Line(Point.ORIGIN, 1, 1);
 		Line n = new Line(Point.ORIGIN, -1, -1);
 		Line a = new Line(Point.ORIGIN, 4, 3);
-
+		Line b = new Line(Point.ORIGIN, 1, 0);
+		
+		System.out.println( b.toString() );
+		b.rotateLine( Math.PI / 4 );
+		System.out.println( b.toString() );
+		
 		assert m.equals(n);
+		assert m.equals(b);
+		assert n.equals(b);
 		assert !a.equals(m);
+		
 
 		System.out.println("Complete.");
 	}
