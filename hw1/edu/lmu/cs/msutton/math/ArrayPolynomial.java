e0c5e510dd1615dcd0d298f4f764d28eae2199f1
garrett.shannon <garrett.shannon@2bf686fb-281d-0410-880b-5de99ea81013>
2006-09-19 05:55:42 +0000

    I have very little what I am doing.  I need to figure out how to use ArrayList

--- hw1/edu/lmu/cs/msutton/math/ArrayPolynomial.java
+++ hw1/edu/lmu/cs/msutton/math/ArrayPolynomial.java
@@ -1,5 +1,22 @@
 package edu.lmu.cs.msutton.math;
 
-public class ArrayPolynomial {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ArrayPolynomial extends Polynomial {
+
+	private List coefficients = new ArrayList();
+
+	public ArrayPolynomial(String coefficients) {
+
+		for (String string : coefficients) {
+			this.coefficients.add(Double.parseDouble(string));
+		}
+	}
+	
+	public ArrayPolynomial(double[] coefficients){
+		for(double[] double : coefficients){
+			this.coefficients.add(double);
+		}
+	}
 }
