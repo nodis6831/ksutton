9396a7bfca4e2aca56beb8e414acc18e0da606ea
garrett.shannon <garrett.shannon@2bf686fb-281d-0410-880b-5de99ea81013>
2006-09-19 05:55:42 +0000

    I have very little what I am doing.  I need to figure out how to use ArrayList

--- trunk/hw1/edu/lmu/cs/msutton/math/Polynomial.java
+++ trunk/hw1/edu/lmu/cs/msutton/math/Polynomial.java
@@ -1,5 +1,24 @@
 package edu.lmu.cs.msutton.math;
 
 public class Polynomial {
+	
+	public Polynomial(){
+		
+	}
 
+	public Polynomial derivative() {
+
+	}
+
+	public int getDegree() {
+
+	}
+
+	public String toString() {
+
+	}
+
+	public double valueAt(double d) {
+
+	}
 }
