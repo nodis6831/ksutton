a7a8384770ae64a2216d5a405bf24eeca5a5dc99
garrett.shannon <garrett.shannon@2bf686fb-281d-0410-880b-5de99ea81013>
2006-09-22 18:03:10 +0000

    *FIRST UPLOAD* This is StudentTest as a JUnit test

---
+++ hw1/edu/lmu/cs/msutton/university/StudentTest.java
@@ -0,0 +1,40 @@
+package edu.lmu.cs.msutton.university;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+/**
+ * A JUnit test class for class Student
+ * 
+ * @author Kelly Sutton
+ * @author Garrett Shannon
+ * 
+ */
+public class StudentTest {
+	@Test
+	public void testGetters() {
+		Student s = new Student("Billy", true, 1987, "USA" );
+		assertEquals(s.getCountry(), "USA");
+		assertEquals(s.getYob(), 1987);
+		assertEquals(s.isMale(), true);
+		assertEquals(s.getName(), "Billy");
+
+	}
+
+	@Test
+	public void testToString() {
+
+	}
+
+	@Test
+	public void testTranscript() {
+
+	}
+
+	@Test
+	public void testUpdateTranscript() {
+
+	}
+}
