071876521792c58b3171c77cbe1296e4878dade2
garrett.shannon <garrett.shannon@2bf686fb-281d-0410-880b-5de99ea81013>
2006-09-24 21:59:00 +0000

    Did some stuff

--- trunk/hw1/edu/lmu/cs/msutton/university/Section.java
+++ trunk/hw1/edu/lmu/cs/msutton/university/Section.java
@@ -3,7 +3,7 @@ package edu.lmu.cs.msutton.university;
 /**
  * 
  * @author Kelly Sutton
- * @author Garrett Shanno
+ * @author Garrett Shannon
  * 
  * 
  */
@@ -14,6 +14,8 @@ public class Section {
 
 	private int section;
 
+	private int creditHours;
+
 	/**
 	 * 
 	 * @param deparment
@@ -22,21 +24,58 @@ public class Section {
 	 *            the course number
 	 * @param section
 	 *            the section number
+	 * @param credits
+	 *            the credit hours
 	 */
-	public Section(String department, int course, int section) {
+	public Section(String department, int course, int section, int creditHours) {
 		this.department = department;
 		this.course = course;
 		this.section = section;
+		this.creditHours = creditHours;
 	}
+
+	/**
+	 * 
+	 * @return the course number
+	 */
+	public int getCourse() {
+		return course;
+	}
+
+	/**
+	 * 
+	 * @return the credits
+	 */
+	public int getCreditHours() {
+		return creditHours;
+	}
+
+	/**
+	 * 
+	 * @return the department name
+	 */
+	public String getDepartment() {
+		return department;
+	}
+
+	/**
+	 * 
+	 * @return the section number
+	 */
+	public int getSection() {
+		return section;
+	}
+
 	/**
 	 * Returns a brief description of the Card. The exact details of the
 	 * representation are unspecified and subject to change, but the following
-	 * may be regarded as typical:
-	 * "Department # Section #" "History 101 Section 02"
+	 * may be regarded as typical: "Department # Section # (Credits)" "History
+	 * 101 Section 02 (3)"
 	 */
-	
-	@Override	
+
+	@Override
 	public String toString() {
-		return department + " " + course + " Section " + section;
+		return department + " " + course + " Section " + section + " ("
+				+ creditHours + ")";
 	}
 }
