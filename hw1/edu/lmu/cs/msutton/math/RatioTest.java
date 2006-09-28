6cf7757f16c8a711912bd0681f56548413694a16
michael.k.sutton <michael.k.sutton@2bf686fb-281d-0410-880b-5de99ea81013>
2006-09-28 04:31:59 +0000

    completed class and interface, added tests

--- hw1/edu/lmu/cs/msutton/math/RatioTest.java
+++ hw1/edu/lmu/cs/msutton/math/RatioTest.java
@@ -1,12 +1,45 @@
 package edu.lmu.cs.msutton.math;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
 /**
+ * Test cases for the Ratio interface and RatioImpl class
  * 
  * @author Garrett Shannon
  * @author Kelly Sutton
- *
+ * 
  */
 
 public class RatioTest {
+
+	@Test
+	public void testGetters() {
+		RatioImpl a = new RatioImpl(100, 1000);
+
+		assertTrue(a.getNum() == 100);
+		assertTrue(a.getDen() == 1000);
+	}
+
+	@Test
+	public void testIdenticalRatios() {
+		RatioImpl a = new RatioImpl(1, 1);
+		RatioImpl b = new RatioImpl(1, 1);
+
+		assertEquals(a, b);
+	}
+
+	@Test
+	public void testZeroDenominator() {
+		try {
+			RatioImpl a = new RatioImpl(1, 0);
+		} catch (IllegalArgumentException e) {
+			assertNotNull(e);
+		}
+	}
 
 }
