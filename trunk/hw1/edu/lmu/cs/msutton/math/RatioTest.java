b87cee8676fb5de25e96eaa490deba89e71aca34
michael.k.sutton <michael.k.sutton@2bf686fb-281d-0410-880b-5de99ea81013>
2006-09-28 05:04:31 +0000

    comment updates

--- trunk/hw1/edu/lmu/cs/msutton/math/RatioTest.java
+++ trunk/hw1/edu/lmu/cs/msutton/math/RatioTest.java
@@ -1,9 +1,8 @@
 package edu.lmu.cs.msutton.math;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
