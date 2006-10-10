7c61ba024a13f465fb6bc61c125c039a905897de
insanemike <insanemike@2bf686fb-281d-0410-880b-5de99ea81013>
2006-10-10 04:37:20 +0000

    ctrl+shift+O

--- hw1/edu/lmu/cs/msutton/math/RatioTest.java
+++ hw1/edu/lmu/cs/msutton/math/RatioTest.java
@@ -1,7 +1,6 @@
 package edu.lmu.cs.msutton.math;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
