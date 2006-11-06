dd0e28c96cb254ec0fa41c8e2e661008dacfdb31
insanemike <insanemike@2bf686fb-281d-0410-880b-5de99ea81013>
2006-11-06 23:38:00 +0000

    copied Ray's code from technocage.com

--- hw2/edu/lmu/cs/msutton/util/Stack.java
+++ hw2/edu/lmu/cs/msutton/util/Stack.java
@@ -1,11 +1,36 @@
 package edu.lmu.cs.msutton.util;
 
 /**
- * 
- * @author Kelly Sutton
- * @author Garret Shannon
+ * A small stack interface.  You can query the size of the stack and
+ * ask whether it is empty, push items, pop items, and peek at the top
+ * item.
  */
+public interface Stack {
 
-public class Stack {
+    /**
+     * Adds the given item to the top of the stack.
+     */
+    void push(Object item);
 
+    /**
+     * Removes the top item from the stack and returns it.
+     * @exception java.util.NoSuchElementException if the stack is empty.
+     */
+    Object pop();
+
+    /**
+     * Returns the top item from the stack without popping it.
+     * @exception java.util.NoSuchElementException if the stack is empty.
+     */
+    Object peek();
+
+    /**
+     * Returns the number of items currently in the stack.
+     */
+    int size();
+
+    /**
+     * Returns whether the stack is empty or not.
+     */
+    boolean isEmpty();
 }
