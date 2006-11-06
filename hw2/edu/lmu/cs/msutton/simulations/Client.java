2ddff725d17c95c6b975246561e5d1fc09f74690
insanemike <insanemike@2bf686fb-281d-0410-880b-5de99ea81013>
2006-11-06 22:13:55 +0000

    initial check-in

---
+++ hw2/edu/lmu/cs/msutton/simulations/Client.java
@@ -0,0 +1,14 @@
+package edu.lmu.cs.msutton.simulations;
+
+/**
+ * A Client interface as copied from the Data Structures book
+ * @author KellySutton
+ * @author Garrett Shannon
+ */
+
+public interface Client {
+
+	public void setStartTime(int t);
+	public void setStopTime(int t);
+	
+}
