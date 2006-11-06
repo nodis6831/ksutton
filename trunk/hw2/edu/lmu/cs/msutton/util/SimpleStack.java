3ae171aa328ebff1172f1c39d6ab556eee5e6529
insanemike <insanemike@2bf686fb-281d-0410-880b-5de99ea81013>
2006-11-06 23:38:00 +0000

    copied Ray's code from technocage.com

--- trunk/hw2/edu/lmu/cs/msutton/util/SimpleStack.java
+++ trunk/hw2/edu/lmu/cs/msutton/util/SimpleStack.java
@@ -1,11 +1,17 @@
 package edu.lmu.cs.msutton.util;
 
+import java.util.LinkedList;
+
 /**
- * 
- * @author Kelly Sutton
- * @author Garret Shannon
+ * A stack class implemented as a wrapper around a java.util.LinkedList.
+ * All stack methods simply delegate to LinkedList methods.
  */
+public class SimpleStack implements Stack {
+    private LinkedList<Object> list = new LinkedList<Object>();
 
-public class SimpleStack {
-
+    public void push(Object item) {list.addFirst(item);}
+    public Object pop() {return list.removeFirst();}
+    public Object peek() {return list.getFirst();}
+    public int size() {return list.size();}
+    public boolean isEmpty() {return list.isEmpty();}
 }
