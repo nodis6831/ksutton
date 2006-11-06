aac50158aafbfdc93185b64dce3a3373b9063fe0
insanemike <insanemike@2bf686fb-281d-0410-880b-5de99ea81013>
2006-11-06 22:21:59 +0000

    initial check-in, as copied from the book

---
+++ trunk/hw2/edu/lmu/cs/msutton/simulations/SimClient.java
@@ -0,0 +1,32 @@
+package edu.lmu.cs.msutton.simulations;
+
+public class SimClient {
+
+	int id, arrivalTime=-1, startTime=-1, stopTime=-1;
+	
+	public SimClient(int id, int t){
+		this.id = id;
+		arrivalTime = t;
+		System.out.println(this + " arrived at time " + t);
+	}
+	
+	public int getStartTIme(){
+		return startTime;
+	}
+	
+	public int getStopTime(){
+		return stopTime;
+	}
+	
+	public void setStartTime(int t){
+		startTime = t;
+	}
+	
+	public void setStopTime(int t){
+		stopTime = t;
+	}
+	
+	public String toString(){
+		return "Client " + id;
+	}
+}
