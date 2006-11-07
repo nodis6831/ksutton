6191e545093486970d6f8e39b6f9c405b1875222
insanemike <insanemike@2bf686fb-281d-0410-880b-5de99ea81013>
2006-11-07 00:28:41 +0000

    Done, now it just needs to be tested... ugh

--- trunk/hw2/edu/lmu/cs/msutton/business/CapitalGainsComputer.java
+++ trunk/hw2/edu/lmu/cs/msutton/business/CapitalGainsComputer.java
@@ -1,5 +1,7 @@
 package edu.lmu.cs.msutton.business;
 
+import java.util.Date;
+
 /**
  * Project 6.1
  * @author Kelly Sutton
@@ -15,17 +17,68 @@ package edu.lmu.cs.msutton.business;
  * and sold queues from data stored in external files.
  */
 
+
+
 public class CapitalGainsComputer {
 
+	public static double main(String[] args){
+		
+		//TODO load data from an external file
+		//fetchData();
+		Transaction[] boughtTransactions = new Transaction[10];	//TMP!
+		Transaction[] soldTransactions = new Transaction[10];	//TMP!
+		
+		LinkedQueue bought = new LinkedQueue();				// #1
+		
+		for (Transaction t: boughtTransactions){			// #2
+			bought.add(t);
+		}
+		
+		LinkedQueue sold = new LinkedQueue(); 				// #3
+			
+		for (Transaction t: soldTransactions){				// #4
+			sold.add(t);
+		}
+		
+		int gains = 0;										// #5
+		
+		while( !bought.isEmpty() && !sold.isEmpty() ){		// #6
+			Transaction buy = (Transaction) bought.first(); // #7
+			Transaction sell = (Transaction) sold.first();
+			
+			if (buy.getShares() < sell.getShares()){					// #8 - we're selling shares
+				gains += buy.getShares() * (sell.getPricePerShare() - buy.getPricePerShare()); // #9
+				bought.remove(); // #10
+				sell.setShares(sell.getShares() - buy.getShares()); // #11 - doesn't this need to be done beforehand?
+			}
+			else{
+				gains += sell.getShares() * (sell.getPricePerShare() - buy.getPricePerShare()); // #12
+				sold.remove(); // #13
+				
+				if( buy.getShares() == sell.getShares()){ // #14
+					bought.remove();
+				}
+				else{
+					buy.setShares(buy.getShares() - sell.getShares());
+				}
+			}
+		}
+		
+		return gains;
+	}
 	
 	/*
 	 * Begin private classes
 	 */
-	private class LinkedQueue implements Queue {
+	private static class LinkedQueue implements Queue {
 		private Node head = new Node(null);
 
 		private int size;
 
+		public LinkedQueue(){
+			size = 0;
+		}
+		
 		public void add(Object object) {
 			head.prev = head.prev.next = new Node(object, head.prev, head);
 			++size;
@@ -54,7 +107,7 @@ public class CapitalGainsComputer {
 			return object;
 
 		}
-		
+
 		public int size(){
 			return size;
 		}
@@ -74,5 +127,56 @@ public class CapitalGainsComputer {
 			this.prev = prev;
 			this.next = next;
 		}
+	}
+	
+	/**
+	 * A Transaction class
+	 *
+	 * @author Kelly Sutton
+	 * @author Garrett Shannon
+	 */
+	private static class Transaction{
+		private int shares;
+		private final double pricePerShare;
+		private final Date transactionDate;
+		
+		public Transaction(int s, double p, Date d){
+			shares = s;
+			pricePerShare = p;
+			transactionDate = d;
+		}
+
+		public Transaction(){
+			shares = 0;
+			pricePerShare = 0;
+			transactionDate = null;
+		}
+		
+		/**
+		 * @return the pricePerShare
+		 */
+		public double getPricePerShare() {
+			return pricePerShare;
+		}
+
+		/**
+		 * @return the shares
+		 */
+		public int getShares() {
+			return shares;
+		}
+
+		public void setShares(int s){
+			shares = s;
+		}
+		
+		/**
+		 * @return the transactionDate
+		 */
+		public Date getTransactionDate() {
+			return transactionDate;
+		}
+		
+		
 	}
 }
