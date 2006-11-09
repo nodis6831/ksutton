a33aea27b51a58b6659f5b7a2c43c26e9db52a88
garrett.shannon <garrett.shannon@2bf686fb-281d-0410-880b-5de99ea81013>
2006-11-09 04:34:38 +0000

    Finished 4.5, 4.8, 4.11

--- hw2/edu/lmu/cs/msutton/util/ListUtil.java
+++ hw2/edu/lmu/cs/msutton/util/ListUtil.java
@@ -1,79 +1,68 @@
 package edu.lmu.cs.msutton.util;
 
 /**
- * Problem 4.5
- * 
  * @author Kelly Sutton
  * @author Garret Shannon
  */
 public class ListUtil {
 
 	/**
+	 * Problem 4.5
+	 * 
+	 * @param list
+	 *            the list
 	 * @return a new list that is a duplicate of the specified list
 	 */
 	public static Node copy(Node list) {
-		
-		/*
-		 * list is only one Node
-		 */
-		if (list.next() == null){
-			return new Node(list.getData());
-		}
-		
-		Node returnNode = new Node(list.getData(), list.next());
-		Node returnListIteratorNode = returnNode;
-		Node iteratorNode = list;
-
-		while (iteratorNode.next() != null) {
-			/*
-			 * generating a completely new Node object linked to a Node object
-			 * created from list.next() and linked to null
-			 */
-			returnListIteratorNode.setNext(new Node(iteratorNode.next().getData(), iteratorNode.next().next()));
-
-			/* move onto to the next Node object */
-			
-			iteratorNode = iteratorNode.next();
-			returnListIteratorNode = returnListIteratorNode.next();
-		}
-		return returnNode;
+		if (list == null)
+			return null;
+		else
+			return new Node(list.getData(), copy(list.next()));
 	}
 
 	/**
 	 * Problem 4.8
 	 * 
+	 * @param list1
+	 *            the first list
+	 * @param list2
+	 *            the second list
 	 * @return a new list that contains a copy of list1, followed by a copy of
 	 *         list2
 	 */
 	public static Node concat(Node list1, Node list2) {
-
-		Node result = copy(list1);
-		// Node b = copy(list2); //Making sure we're not playing with the
-		// original Nodes in memory
-
-		Node iteratorNode = result;
-
-		while (true) {
-
-			if (iteratorNode.next() == null) {
-				iteratorNode.setNext(copy(list2)); // This madness appends a
-				// copy of list2
-				break;
-			}
-			iteratorNode = iteratorNode.next();
-		}
-
-		return result;
+		if (list1 == null && list2 == null)
+			return null;
+		else if (list1 == null && list2 != null)
+			return copy(list2);
+		else if (list1 != null && list2 == null)
+			return copy(list1);
+		else
+			return new Node(list1.getData(), concat(list1.next(), list2));
 	}
 
+	/**
+	 * Problem 4.11
+	 * 
+	 * @param list1
+	 *            the first list
+	 * @param list2
+	 *            the second list
+	 * @return a new list which is a combination of list1 and list2 but in
+	 *         ascending order provided list1 and list2 are already in ascending
+	 *         order
+	 */
 	public static Node merged(Node list1, Node list2) {
-		
-		Node result = new Node(list1.getData() <= list2.getData() ? list1.getData() : list2.getData());
-		//figure out which list has the smallest number
-		while (list1.next() != null | list2.next() != null) {
-			result.setData(list1.getData() <= list2.getData() ? list1.getData() : list2.getData());
+		if (list1 == null && list2 == null)
+			return null;
+		else if (list1 == null && list2 != null)
+			return copy(list2);
+		else if (list1 != null && list2 == null)
+			return copy(list1);
+		else if (list1.getData() <= list2.getData()) {
+			return new Node(list1.getData(), merged(list1.next(), list2));
+		} else {
+			return new Node(list2.getData(), merged(list2.next(), list1));
 		}
-
-		return result;
 	}
 }
