b0dc742c171207fcf0402d624f8fc7b0cd7e3157
insanemike <insanemike@2bf686fb-281d-0410-880b-5de99ea81013>
2006-11-09 06:48:15 +0000

    initial check-in, from technocage.com

---
+++ trunk/hw2/edu/lmu/cs/msutton/simulations/BoundedQueue.java
@@ -0,0 +1,59 @@
+package edu.lmu.cs.msutton.simulations;
+
+import java.util.NoSuchElementException;
+
+/**
+ * An implementation of a queue using a fixed, non-expandable array whose
+ * capacity is set in its constructor. From technocage.com
+ */
+public class BoundedQueue implements Queue {
+	private Object[] array;
+
+	private int size = 0;
+
+	private int head = 0; // index of the current front item, if one exists
+
+	private int tail = 0; // index of next item to be added
+
+	public BoundedQueue(int capacity) {
+		array = new Object[capacity];
+	}
+
+	public void enqueue(Object item) {
+		if (size == array.length) {
+			// throw new IllegalStateException("Cannot add to full queue");
+			// We've got plenty of memory, so we'll just created a new array
+			// twice the size
+			array = new Object[this.size() * 2];//TODO whoops, we'll need to copy everything in
+		}
+		array[tail] = item;
+		tail = (tail + 1) % array.length;
+		size++;
+	}
+
+	public Object dequeue() {
+		if (size == 0) {
+			throw new NoSuchElementException("Cannot remove from empty queue");
+		}
+		Object item = array[head];
+		array[head] = null;
+		head = (head + 1) % array.length;
+		size--;
+		return item;
+	}
+
+	public Object peek() {
+		if (size == 0) {
+			throw new NoSuchElementException("Cannot peek into empty queue");
+		}
+		return array[size - 1];
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	public int size() {
+		return size;
+	}
+}
