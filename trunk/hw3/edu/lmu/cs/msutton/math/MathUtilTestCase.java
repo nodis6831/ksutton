05087c31b6d4af26cdfd3d7b439dd33f5aa22481
insanemike <insanemike@2bf686fb-281d-0410-880b-5de99ea81013>
2006-11-26 02:30:37 +0000

    Problem 10.11 complete!

--- trunk/hw3/edu/lmu/cs/msutton/math/MathUtilTestCase.java
+++ trunk/hw3/edu/lmu/cs/msutton/math/MathUtilTestCase.java
@@ -1,7 +1,18 @@
 package edu.lmu.cs.msutton.math;
 
 import junit.framework.TestCase;
+import static edu.lmu.cs.msutton.math.MathUtil.intToBinary;
+
+import org.junit.Test;
 
 public class MathUtilTestCase extends TestCase {
 
+	@Test
+	public void testIntToBinary(){
+		assertEquals(intToBinary(13),"1101");
+		assertEquals(intToBinary(8), "1000");
+		assertEquals(intToBinary(1), "1");
+		assertEquals(intToBinary(0), "0");
+	}
+	
 }
