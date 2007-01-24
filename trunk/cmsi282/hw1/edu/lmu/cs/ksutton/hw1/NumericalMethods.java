efe7457cb33e9895a2c664e0d9e488c0e681a4bd
michael.k.sutton <michael.k.sutton@2bf686fb-281d-0410-880b-5de99ea81013>
2007-01-24 05:02:38 +0000

    fib done, needs testing

--- trunk/cmsi282/hw1/edu/lmu/cs/ksutton/hw1/NumericalMethods.java
+++ trunk/cmsi282/hw1/edu/lmu/cs/ksutton/hw1/NumericalMethods.java
@@ -4,27 +4,32 @@ import java.math.BigInteger;
 
 public class NumericalMethods {
 
-	public static BigInteger fibonacci(int n){
-		
+	/**
+	 * 
+	 * @param n
+	 *            index that is 0, 1, 2
+	 * @return the nth number in the sequence: 0, 1, 1, 2, 3, 5, 8, 13, ...
+	 */
+	public static BigInteger fibonacci(int n) {
+
 		BigInteger a = new BigInteger("0");
 		BigInteger b = new BigInteger("1");
 		BigInteger f = new BigInteger("-1");
-		
-		if (n <= 0){
+
+		if (n < 0) {
 			throw new IllegalArgumentException();
 		}
-			
-		
-		if (n == 1){
+
+		if (n == 0) {
 			return a;
 		}
-		
-		else if (n == 2){
+
+		else if (n == 1) {
 			return b;
 		}
-		
-		else{ //n > 2
-			for (int i = 0; i <= n; i++){
+
+		else { // n > 2
+			for (int i = 0; i <= n - 2; i++) {
 				f = a.add(b);
 				a = b;
 				b = f;
