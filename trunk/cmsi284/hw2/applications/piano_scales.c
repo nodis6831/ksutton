ba9b7961e140073801dec2cca398f26dd3e3b2f5
michael.k.sutton <michael.k.sutton@2bf686fb-281d-0410-880b-5de99ea81013>
2007-02-07 00:25:54 +0000

    figure out how to take in command line arguments other than that we're done

---
+++ trunk/cmsi284/hw2/applications/piano_scales.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+	
+	int tmp_ki = 3; //key of C
+	
+	char* keys[13] = { "A" , "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#" };
+	int major[8] = { 0, 2, 4, 5, 7, 9, 11, 12 }; //these arrays represent the number of tones away from key we're in
+	int minor[8] = { 0, 2, 3, 5, 7, 9, 11, 12 }; 
+	
+	int i;
+	printf("%s major: ", keys[tmp_ki]);
+	for (i = 0; i < 8; i++){
+	    printf( "%s ", keys[ (tmp_ki + major[i]) % 12]); 	
+	}
+	printf("\n");
+
+	printf("%s minor: ", keys[tmp_ki]);
+	for (i = 0; i < 8; i++){
+	    printf( "%s ", keys[ (tmp_ki + minor[i]) % 12]); 	
+	}
+	printf("\n");
+
+	
+	return 0;	
+}
