4c77bc854ab8c30d3aa0c00050249edf8d05a1d9
michael.k.sutton <michael.k.sutton@2bf686fb-281d-0410-880b-5de99ea81013>
2007-02-09 07:21:15 +0000

    check in at the end of the night

--- cmsi282/hw2/edu/lmu/cs/ksutton/cmsi282/hw2/BucketSort.java
+++ cmsi282/hw2/edu/lmu/cs/ksutton/cmsi282/hw2/BucketSort.java
@@ -1,5 +1,20 @@
 package edu.lmu.cs.ksutton.cmsi282.hw2;
 
+import java.util.ArrayList;
+
 public class BucketSort {
+
+	public static void main(String args[]) {
+
+		//readFromFile(args[0]);
+		
+		ArrayList masterlist = new ArrayList(10);
+		
+		masterlist.add(new ArrayList(5));
+		masterlist.add(new ArrayList(5));
+		masterlist.add(new ArrayList(5));
+		
+		ArrayList list = (ArrayList) masterlist.get(1);
+	}
 
 }
