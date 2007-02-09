c2c41d2db2c746950b795badb098e3468ea8f81f
michael.k.sutton <michael.k.sutton@2bf686fb-281d-0410-880b-5de99ea81013>
2007-02-09 05:39:59 +0000

    getting closer with #3 but still a lot of work to do

--- cmsi282/hw2/edu/lmu/cs/ksutton/cmsi282/hw2/IntegerHeapSort.java
+++ cmsi282/hw2/edu/lmu/cs/ksutton/cmsi282/hw2/IntegerHeapSort.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 
 public class IntegerHeapSort {
 
@@ -13,20 +12,44 @@ public class IntegerHeapSort {
 		// File reading taken from a tutorial at:
 		// http://www.javapractices.com/Topic42.cjp
 
-		System.out.println("The file name is " + args[0]);
+		//readFromFile(args[0]);
+
+		IntHeap heap = new IntHeap(12);
+		
+		heap.add(13);
+		heap.add(4);
+		heap.add(81);
+		heap.add(39);
+		heap.add(1);
+		heap.add(8);
+		heap.add(49);
+		heap.add(2);
+		heap.add(3);
+		heap.add(13);
+		heap.add(88);
+		heap.add(99);
+		
+		System.out.println(heap.toString());
+		
+		
+	}
+
+	private static IntHeap readFromFile(String f) {
+
+		System.out.println("The file name is " + f);
 		BufferedReader input = null;
 
 		try {
 
 			IntHeap heap;
 
-			input = new BufferedReader(new FileReader(args[0]));
+			input = new BufferedReader(new FileReader(f));
 			String line = null;
 
 			int i = 0;
 			while ((line = input.readLine()) != null) { // we're just going to
-														// count the lines this
-														// time through
+				// count the lines this
+				// time through
 				i++;
 			}
 
@@ -41,7 +64,7 @@ public class IntegerHeapSort {
 
 			// now we reopen the file and load it into our array
 
-			input = new BufferedReader(new FileReader(args[0]));
+			input = new BufferedReader(new FileReader(f));
 			line = null;
 
 			while ((line = input.readLine()) != null) {
@@ -49,6 +72,7 @@ public class IntegerHeapSort {
 			}
 
 			System.out.println(heap.toString());
+			return heap;
 
 		}
 
@@ -70,150 +94,7 @@ public class IntegerHeapSort {
 				e.printStackTrace();
 			}
 		}
+		return null;
 
 	}
-}
-
-class IntHeap {
-
-	private int[] a; // the heap
-
-	private int size;
-
-	public IntHeap(int s) {
-
-		a = new int[s];
-		size = s;
-
-	}
-
-	/**
-	 * Adds an int to the heap and readjusts the heap
-	 * 
-	 * @param x
-	 *            The int to add to the heap
-	 */
-	public void add(int x) {
-
-		a[size + 1] = x;
-
-		size++;
-		reheap_up();
-
-	}
-
-	/**
-	 * 
-	 * @return The top-most element of the heap
-	 */
-	public int remove() {
-
-		int top = a[1];
-
-		if (a[2] > a[3]) {
-			a[1] = a[2];
-		}
-
-		else
-			// a[2] < a[3]
-			a[1] = a[3];
-
-		size--;
-		reheap_down();
-
-		return top;
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return Arrays.toString(a);
-	}
-
-	/**
-	 * Recursive method that reorganizes the heap, should be called every time
-	 * an object is added or removed from the heap.
-	 */
-	private void reheap_down() {
-
-		int i = 0;
-
-		while (2 * i + 1 < size) {
-			int child = 2 * i + 1;
-
-			// since we're pushing a number down, we want to do the swapping
-			// with the smallest number
-			if (child < size && a[child] > a[child + 1])
-				child++;
-
-			// the number is as far down as it needs to go
-			if (a[i] < a[child])
-				break;
-
-			else {
-				swapInts(child, i);// we've found the smallest numbered child
-									// and we're swapping
-				i = child;// now we move on to see if we need to go any lower
-			}
-		}
-
-	}
-
-	/**
-	 * Recursive method that reorganizes the heap, should be called every time
-	 * an object is added or removed from the heap.
-	 */
-	private void reheap_up() {
-
-		int i = size - 1;
-
-		while (i > 1) {
-			if (parent(i) > a[i]) {
-				swapInts(i, indexOfParent(i));
-			}
-
-			i = indexOfParent(i);
-		}
-
-	}
-
-	/**
-	 * 
-	 * @param i
-	 *            The index of the item in question
-	 * @return The index of the parent
-	 */
-	private int indexOfParent(int i) {
-		return i / 2;
-	}
-
-	/**
-	 * 
-	 * @param i
-	 *            The index of the child
-	 * @return The parent's data
-	 */
-	private int parent(int i) {
-		return a[indexOfParent(i)];
-	}
-
-	/**
-	 * Swaps the values at the two indeces of the array
-	 * 
-	 * @param i
-	 * @param j
-	 */
-	private void swapInts(int i, int j) {
-
-		int tmp = a[i];
-		a[i] = a[j];
-		a[j] = tmp;
-
-	}
-
 }
