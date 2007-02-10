90a55673aea822a02a9aa7fcb60de9e239387f49
michael.k.sutton <michael.k.sutton@2bf686fb-281d-0410-880b-5de99ea81013>
2007-02-10 23:04:09 +0000

    BucketSort is done!

--- trunk/cmsi282/hw2/edu/lmu/cs/ksutton/cmsi282/hw2/NumberReaderTester.java
+++ trunk/cmsi282/hw2/edu/lmu/cs/ksutton/cmsi282/hw2/NumberReaderTester.java
@@ -2,6 +2,7 @@ package edu.lmu.cs.ksutton.cmsi282.hw2;
 
 import static junit.framework.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.junit.Test;
@@ -11,11 +12,21 @@ public class NumberReaderTester {
 	@Test
 	public void testDoubleReader(){
 		
-		double[] a = NumberReader.readDoublesFromFile("/Users/mike/Documents/workspace/cmsi282hw2/edu/lmu/cs/ksutton/cmsi282/hw2/doubles.txt");
+		ArrayList a = NumberReader.readDoublesFromFile("/Users/mike/Documents/workspace/cmsi282hw2/edu/lmu/cs/ksutton/cmsi282/hw2/doubles.txt");
 		
-		double[] b = { 12.3, 1.0, 18.9, 45.6, 37.6, 99.99, 87.4, 20.0, 61.4, 78.2};
+		ArrayList b = new ArrayList();
+		b.add(12.3);
+		b.add(1.0);
+		b.add(18.9);
+		b.add(45.6);
+		b.add(37.6);
+		b.add(99.99);
+		b.add(87.4);
+		b.add(20.0);
+		b.add(61.4);
+		b.add(78.2);
 		
-		assertEquals(Arrays.toString(a), Arrays.toString(b));
+		assertEquals(a.toString(), b.toString());
 	}
 		
 }
