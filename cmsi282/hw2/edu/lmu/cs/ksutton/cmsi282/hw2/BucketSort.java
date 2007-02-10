1c6424d0d21a269ecf27532b2a37fefca4c077e5
michael.k.sutton <michael.k.sutton@2bf686fb-281d-0410-880b-5de99ea81013>
2007-02-10 23:04:09 +0000

    BucketSort is done!

--- cmsi282/hw2/edu/lmu/cs/ksutton/cmsi282/hw2/BucketSort.java
+++ cmsi282/hw2/edu/lmu/cs/ksutton/cmsi282/hw2/BucketSort.java
@@ -6,58 +6,67 @@ import java.util.Arrays;
 public class BucketSort {
 
 	public static void main(String args[]) {
-		
+
 		NumberReader.readDoublesFromFile(args[0]);
-		ArrayList<Double> masterlist = NumberReader.readDoublesFromFile(args[0]);;
-		
-		//all of the doubles will be 0.0 <= x < 100.0
+		ArrayList<Double> masterlist = NumberReader
+				.readDoublesFromFile(args[0]);
+		;
+
+		// all of the doubles will be 0.0 <= x < 100.0
 		double rangeOfBucket = 100.0 / masterlist.size();
-		
-		//creating a new ArrayList of n buckets
+
+		// creating a new ArrayList of n buckets
 		ArrayList[] buckets = new ArrayList[masterlist.size()];
 		Arrays.fill(buckets, new ArrayList());
-		
-		
+
 		double d;
-		for(int i = 0; i < masterlist.size(); i++){
-			d = (Double) masterlist.get(i); 
-			buckets[(int)( d / rangeOfBucket )].add(d); //this mess discovers the appropriate bucket and dumps d in it
+		for (int i = 0; i < masterlist.size(); i++) {
+			d = (Double) masterlist.get(i);
+			buckets[(int) (d / rangeOfBucket)].add(d); // this mess discovers
+														// the appropriate
+														// bucket and dumps d in
+														// it
 		}
-		
-		//now we sort and spit out the list as we sort
+
+		// now we sort and spit out the list as we sort
 		int k;
-		for (k = 0; k < buckets.length - 1; k++);{
-			buckets[k] = insertionSort(buckets[k]); //insert sort each bucket
+		for (k = 0; k < buckets.length - 1; k++)
+			;
+		{
+			buckets[k] = insertionSort(buckets[k]); // insert sort each bucket
 			System.out.println(buckets[k].toString() + "\n");
 		}
 
 	}
 
-	
-	
 	/**
-	 * Simple insert sort method
-	 * Taken from the pseucode on the Insert Sort Wikipedia page
-	 * http://en.wikipedia.org/wiki/Insert_sort
-	 * @param a The list to be sorted
-	 * @return An ArrayList object sorted in ascending order
+	 * Modified versino of InsertionSort found at
+	 * http://www.samspublishing.com/articles/article.asp?p=31526&seqNum=4&rl=1
+	 * 
+	 * @param a
+	 * @return
 	 */
-	private static ArrayList insertionSort(ArrayList a){
-		
-		int i = 0;//the space in the list
-		while (i < a.size() - 1) {//start filling in the smallest elements first
+	public static ArrayList insertionSort(ArrayList a) {
+
+		int in, out;
+
+		for (out = 1; out < a.size(); out++) 
+		{
+			double temp = (Double) a.get(out); // remove marked item
 			
-		    int j = a.size() - 1;
-			while (j >= 0 && (Double) a.get(j) > (Double) a.get(i)) {//while the element we're dealing with, i, is bigger than j
-				a.set(j, a.get(j-1)); //percolate the element down
-				j--;
+			in = out; // start shifts at out
+			
+			// until one is smaller,
+			while (in > 0 && (Double) a.get(in - 1) >= temp) 
+			{
+				a.set(in, a.get(in - 1));// shift item right
+				in--; // go left one position
 			}
 			
-			a.set(i+1, a.get(i));
-	        i++;
-	     }
+			a.set(in, temp); // insert marked item
+		} 
 
 		return a;
-		
-	}
+	} 
+
 }
