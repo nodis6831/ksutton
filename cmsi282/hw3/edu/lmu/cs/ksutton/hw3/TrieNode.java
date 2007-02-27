2a69c0cb6f403c0d110395c6384c40bc71f163f2
michael.k.sutton <michael.k.sutton@2bf686fb-281d-0410-880b-5de99ea81013>
2007-02-27 01:10:04 +0000

    Making progress

--- cmsi282/hw3/edu/lmu/cs/ksutton/hw3/TrieNode.java
+++ cmsi282/hw3/edu/lmu/cs/ksutton/hw3/TrieNode.java
@@ -1,58 +1,115 @@
 package edu.lmu.cs.ksutton.hw3;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 
 public class TrieNode {
 
-	private char c;
-	private HashMap<Character, TrieNode> children;
+	private Character c;
+
+	private ArrayList<TrieNode> children;
+
 	private TrieNode parent;
+
 	private boolean end;
-	
+
+	private int count = 0; // For Concordance
+
 	/**
 	 * The root node constructor
 	 */
-	public TrieNode(){
-		//TODO do we need to initialize c?
-		children = null;
-		parent = null; //root node
+	public TrieNode() {
+		// TODO do we need to initialize c?
+		c = null;
+		children = new ArrayList<TrieNode>(0);
+		parent = null; // root node
 		end = false;
 	}
-	
-	public TrieNode(char character, TrieNode p){
+
+	public TrieNode(char character, TrieNode p) {
 		c = character;
-		children = null;
+		children = new ArrayList<TrieNode>(0);
 		parent = p;
 		end = false;
 	}
-	
-	public TrieNode(char character, TrieNode p, boolean e){
+
+	public TrieNode(char character, TrieNode p, boolean e) {
 		c = character;
-		children = null;
+		children = new ArrayList<TrieNode>(0);
 		parent = p;
 		end = true;
 	}
-	
+
 	/**
 	 * A recursive add to add a String to our Trie
-	 * @param s The string to be added
+	 * 
+	 * @param s
+	 *            The string to be added
 	 */
-	public void add(String s){
-		
-		//Last character of a word
-		if (s.length() == 1)
+	public void add(String s) {
+
+		// Last character of a word
+		if (s.length() == 0) {
 			end = true;
-		
-		else if (s.length() > 1 || parent != null){
-			c = s.charAt(0);//reading the current character
-			
-			//Create a child if it doesn't already exist
-			if(children.get(s.charAt(0)) == null)
-				children.put(s.charAt(0), new TrieNode(s.charAt(0), this));
-				
-				children.get(s.charAt(0)).add(s.substring(1));
-			
-		}	
+			count++;
+		}
+
+		else {
+			Character curChar = s.charAt(0);
+
+			// we need to create the first child then recurse
+			if (/* parent != null && */getChild(curChar) == null) {
+				children.add(new TrieNode(curChar, this));
+
+			}
+
+			/*
+			 * else if (s.length() > 1 && parent != null) { // Create a child if
+			 * it doesn't already exist if (getChild(c) == null){
+			 * children.add(new TrieNode(c, this)); }
+			 * 
+			 * //Recurse getChild(c).add(s.substring(1)); }
+			 */
+
+			getChild(curChar).add(s.substring(1));
+		}
+	}
+
+	/**
+	 * A recursive method to output the frequency of a word inside the Trie
+	 * 
+	 * @param s
+	 *            The string to find inside the Trie
+	 * @return The number of times the word occurs in the Trie
+	 */
+	public int find(String s) {
+		return 0;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+
+		// TODO output all words in the Trie
+		String toReturn = "";
+
+		if (end == true) // we've reached a leaf
+			return Character.toString(c) + "\n";
+
+		else {
+			for (int i = 0; i < children.size(); i++) {
+				if (parent != null) // child node
+					toReturn += Character.toString(c)
+							+ children.get(i).toString();
+				else
+					toReturn += children.get(i).toString();
+			}
+		}
+
+		return toReturn;
 	}
 
 	/**
@@ -65,8 +122,28 @@ public class TrieNode {
 	/**
 	 * @return The children of the current TrieNode
 	 */
-	public HashMap<Character, TrieNode> getChildren() {
+	public ArrayList<TrieNode> getChildren() {
 		return children;
+	}
+
+	/**
+	 * A brute force method that parses through the list of children in hopes of
+	 * finding a match. Not the most efficient thing in the world, but if we
+	 * assume a 26 character alphabet, we should be okay.
+	 * 
+	 * @return The child with the specific character, null otherwise
+	 */
+	public TrieNode getChild(Character c) {
+
+		if (c == null)
+			return null;
+
+		for (int i = 0; i < children.size(); i++) {
+			if (children.get(i).getC() == c)
+				return children.get(i);
+		}
+
+		return null;// we didn't find what we were looking for
 	}
 
 	/**
@@ -82,6 +159,5 @@ public class TrieNode {
 	public TrieNode getParent() {
 		return parent;
 	}
-	
-	
+
 }
