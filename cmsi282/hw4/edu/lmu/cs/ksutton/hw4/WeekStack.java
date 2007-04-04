552cf243a44479589addf36358770839f00094c5
michael.k.sutton <michael.k.sutton@2bf686fb-281d-0410-880b-5de99ea81013>
2007-04-04 04:09:29 +0000

    Cut out a few hundred lines of obsolete code

--- cmsi282/hw4/edu/lmu/cs/ksutton/hw4/WeekStack.java
+++ cmsi282/hw4/edu/lmu/cs/ksutton/hw4/WeekStack.java
@@ -8,4 +8,22 @@ import java.util.Stack;
  */
 public class WeekStack extends Stack<DayStack>{
 
+	/* (non-Javadoc)
+	 * @see java.util.Vector#toString()
+	 */
+	@Override
+	public synchronized String toString() {
+	
+		String toReturn = "";
+		while (this.size() >= 1) {
+			toReturn += "Day " + Math.abs((this.size() - 8)) + "\n";
+
+			DayStack currentDay = this.pop();
+			while (currentDay.size() >= 1) {	
+				RowStack row = currentDay.pop();
+				toReturn += row + "\n";
+			}
+		}
+		return toReturn;
+	}
 }
