800f5e2522cbbc1ba1dfc74d2e15ebfa1145bf77
michael.k.sutton <michael.k.sutton@2bf686fb-281d-0410-880b-5de99ea81013>
2007-04-04 00:21:47 +0000

    initial check-in

---
+++ cmsi282/hw4/edu/lmu/cs/ksutton/hw4/WeekStack.java
@@ -0,0 +1,11 @@
+package edu.lmu.cs.ksutton.hw4;
+
+import java.util.Stack;
+
+/**
+ * For the Kirkman Schoolgirl problem. This holds DayStacks.
+ * @author Kelly Sutton
+ */
+public class WeekStack extends Stack<DayStack>{
+
+}
