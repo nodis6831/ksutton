ac56332c9c7ada757f231c974d32218b0a1925fd
michael.k.sutton <michael.k.sutton@2bf686fb-281d-0410-880b-5de99ea81013>
2007-04-04 02:48:34 +0000

    before an initial run

--- trunk/cmsi282/hw4/edu/lmu/cs/ksutton/hw4/SchoolSolver.java
+++ trunk/cmsi282/hw4/edu/lmu/cs/ksutton/hw4/SchoolSolver.java
@@ -31,40 +31,86 @@ public class SchoolSolver {
 	 * and j = {0, 1, 2})
 	 */
 	// private static RowStack rows;
+	
 	/**
-	 * girlqueue is the queue to which we add and take girls from depending on
-	 * whether or not they fit into the slots correctly
+	 * The week object is a stack of stacks of stacks. Each week holds its own
+	 * days which then holds in own rows stack. The stacks inside a stack make
+	 * it easy to backtrack.
 	 */
 
-	protected static SchoolgirlQueue girlQueue;
-
-	/**
-	 * The days object is a stack of stacks. Each "day" holds its own row stack.
-	 * The stacks inside a stack make it easy to backtrack.
-	 */
-	private static DayStack days = new DayStack();
+	private static WeekStack week = new WeekStack();
 
 	private static int nodes = 0;
 
 	public static void main(String[] args) {
 
 		hasStoodNextTo = fillMatrixWithFalse();
-		usedToday = returnArrayFullOfFalse(15);
 
-		while (days.size() <= 7) {
+		boolean dayFailed = false;
+
+		while (week.size() <= 7) { // 7 days
 
 			// Add a new empty RowStack to our DayStack
-			days.push(new RowStack());
+			week.push(new DayStack(15));
 
-			while (days.peek().size() < 5) {
+			DayStack currentDay = week.peek();
+			SchoolgirlQueue girlQueue = currentDay.getSchoolgirlQueue();
+			while (currentDay.size() < 5) { // 5 rows
+
+				// we need to pop back
+				//  this should be rare or should never happen
+				if (dayFailed) {
+					week.pop();
+					week.peek().peek().pop();
+					week.peek().peek().add(week.peek().getSchoolgirlQueue().dequeue());
+					
+					dayFailed = false;
+					break;
+				}
 
 				// try to add three schoolgirls
-				days.peek().push(tryToAddNewRow(new Integer[] { 0, 1, 2 }));
+				// days.peek().push(tryToAddNewRow(new Integer[] { 0, 1, 2 }));
+
+				RowStack currentRow = currentDay.peek();
+
+				while (currentRow.size() < 3) { // 3 girls to a row
+
+					// we've already been through the girls once
+					if (girlQueue.getFlag() == true) {
+
+						// the current row is no good
+						if (currentDay.size() == 1) {
+							dayFailed = true;
+						}
+
+						girlQueue.enqueue(currentDay.pop());
+
+						// step one back one row and pop
+						girlQueue.enqueue(currentDay.peek().pop());
+
+						// when we break, we'll reset the currentRow and try to
+						// add the next girl in the queue. Hopefully we can
+						// avoid
+						// a freak bug where we get caught in an infinite loop
+						break;
+					}
+
+					currentRow.push(girlQueue.dequeue());
+
+					if (isRowOkay(currentRow)) { // march on
+						girlQueue.setFlag(false);
+					}
+
+					else { // pop off and try adding the next
+						girlQueue.enqueue(currentRow.pop());
+					}
+				}
 			}
-			usedToday = returnArrayFullOfFalse(15);
+			// usedToday = returnArrayFullOfFalse(15);
+			girlQueue = new SchoolgirlQueue(15);
 		}
 
-		printResults();
+		printResults(); // TODO update printResults
 	}
 
 	/**
@@ -107,6 +153,7 @@ public class SchoolSolver {
 	 *            An int array of size 3
 	 * @return
 	 */
+	/*
 	private static Integer[] tryToAddNewRow(Integer[] newRow) {
 
 		Integer[] attempt;
@@ -174,6 +221,7 @@ public class SchoolSolver {
 
 		return null;
 	}
+	*/
 
 	private static boolean[][] fillMatrixWithFalse() {
 		boolean[][] tmp = new boolean[15][15];
@@ -195,10 +243,13 @@ public class SchoolSolver {
 
 		return tmp;
 	}
-
+	
 	private static void printResults() {
 
-		while (days.size() > 1) {
+		System.out.println(week);
+		
+		/*
+		while (week.size() > 1) {
 			System.out.println("Day " + Math.abs((days.size() - 8)));
 
 			while (days.peek().size() > 0) {
@@ -209,8 +260,10 @@ public class SchoolSolver {
 		}
 
 		System.out.println(nodes);
+		*/
 	}
 
+	/*
 	private static void loadCurrentDaysPlacedSchoolgirls() {
 		usedToday = returnArrayFullOfFalse(15);
 		for (Integer[] a : days.peek()) {
@@ -218,5 +271,35 @@ public class SchoolSolver {
 				usedToday[i] = true;
 			}
 		}
+	}
+	*/
+
+	/**
+	 * This messy private helper method returns whether or not r is a valid row
+	 * 
+	 * @param r
+	 * @return
+	 */
+	private static boolean isRowOkay(RowStack r) {
+
+		if (r.size() == 3) {
+			if (hasStoodNextTo[r.elementAt(0).getIndex()][r.elementAt(1)
+					.getIndex()]
+					|| hasStoodNextTo[r.elementAt(1).getIndex()][r.elementAt(2)
+							.getIndex()]
+					|| hasStoodNextTo[r.elementAt(0).getIndex()][r.elementAt(2)
+							.getIndex()])
+				return false;
+		}
+
+		else if (r.size() == 2) {
+			if (hasStoodNextTo[r.elementAt(0).getIndex()][r.elementAt(1)
+					.getIndex()]
+					|| hasStoodNextTo[r.elementAt(1).getIndex()][r.elementAt(0)
+							.getIndex()])
+				return false;
+		}
+
+		return true;
 	}
 }
